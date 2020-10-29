1706d9e5a5879ddb0d72e8362c20b6dffb5c6c57
John Anglin <anglin@paramountcheese.com>
2020-10-29 21:23:42 +0300

    XploreChq201029

--- ztestenv/xplore.py
+++ ztestenv/xplore.py
@@ -1,3 +1,10 @@
 
-content = "Hello World"
-print (content)
+list_0 = ["apple","pear","grape"]
+print (list_0)
+list_cpy = list_0[:]
+
+# item = list_cpy.pop()
+# print (item)
+list_cpy.append("melon")
+print (list_cpy)
+print (list_0)
